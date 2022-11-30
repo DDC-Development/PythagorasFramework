f03831565e46aea827cd08a221dceba2f4fb773c
LukeChemeriov <dgm.bailey@outlook.com>
2022-11-30 13:03:58 +0300

    Fixed discrepancy between C++ and C

--- src/Pythagoras.h
+++ src/Pythagoras.h
@@ -5,32 +5,26 @@
 
 #include <cmath>
 
-namespace Pythagoras
+struct Pythagoras
 {
-    class TheoremFunctions
+    float CalculateC(float A, float B)
     {
-    public:
-        TheoremFunctions(){}
-        ~TheoremFunctions(){}
-        float CalculateC(float A, float B)
-        {
-            // c^2 = a^2 + b^2
-            return sqrt((A*A)+(B*B));
-            // This returns the value of C, not the value of a^2 + b^2
-        }
-        float CalculateA(float C, float B)
-        {
-            // c^2 = a^2 + b^2
-            // c^2 - b^2 = a^2
-            return sqrt((C*C)-(B*B));
-            // This returns the value of A, not the value of c^2 - b^2
-        }
-        float CalculateB(float C, float A)
-        {
-            // c^2 = a^2 + b^2
-            // c^2 - a^2 = b^2
-            return sqrt((C*C)-(A*A));
-            // This returns the value of B, not the value of c^2 - a^2
-        }
-    };
-}
+        // c^2 = a^2 + b^2
+        return sqrt((A*A)+(B*B));
+        // This returns the value of C, not the value of a^2 + b^2
+    }
+    float CalculateA(float C, float B)
+    {
+        // c^2 = a^2 + b^2
+        // c^2 - b^2 = a^2
+        return sqrt((C*C)-(B*B));
+        // This returns the value of A, not the value of c^2 - b^2
+    }
+    float CalculateB(float C, float A)
+    {
+        // c^2 = a^2 + b^2
+        // c^2 - a^2 = b^2
+        return sqrt((C*C)-(A*A));
+        // This returns the value of B, not the value of c^2 - a^2
+    }
+};
---
+++ src/Pythagoras.hpp
@@ -0,0 +1,36 @@
+// Pythagoras Framework
+// (C) DDC Dev, 2022
+
+// Pythagoras namespace and struct
+
+#include <cmath>
+
+namespace Pythagoras
+{
+    class TheoremFunctions
+    {
+    public:
+        TheoremFunctions(){}
+        ~TheoremFunctions(){}
+        float CalculateC(float A, float B)
+        {
+            // c^2 = a^2 + b^2
+            return sqrt((A*A)+(B*B));
+            // This returns the value of C, not the value of a^2 + b^2
+        }
+        float CalculateA(float C, float B)
+        {
+            // c^2 = a^2 + b^2
+            // c^2 - b^2 = a^2
+            return sqrt((C*C)-(B*B));
+            // This returns the value of A, not the value of c^2 - b^2
+        }
+        float CalculateB(float C, float A)
+        {
+            // c^2 = a^2 + b^2
+            // c^2 - a^2 = b^2
+            return sqrt((C*C)-(A*A));
+            // This returns the value of B, not the value of c^2 - a^2
+        }
+    };
+}
